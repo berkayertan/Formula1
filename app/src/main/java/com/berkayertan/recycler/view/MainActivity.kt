21f8d76a35819ad84add695ed15b6d58550964dd
Berkay <berkayertan12@gmail.com>
2023-04-24 21:18:17 +0300

    -Added navigation header

--- app/src/main/java/com/berkayertan/recycler/view/MainActivity.kt
+++ app/src/main/java/com/berkayertan/recycler/view/MainActivity.kt
@@ -35,6 +35,7 @@ class MainActivity : AppCompatActivity() {
                 R.id.stand-> setCurrentFragment(driverstand)
                 R.id.constructor-> setCurrentFragment(constructorstand)
             }
+            binding.drawerLayout.closeDrawers()
             true
         }
 
